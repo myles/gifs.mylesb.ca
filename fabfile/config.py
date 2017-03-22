0d28ff9faef858ad088351f7c1eca721393c3c1c
Myles Braithwaite <me@mylesbraithwaite.com>
2017-03-22 10:38:24 -0400

    :rocket: Fixed up some deploy issues.

--- fabfile/config.py
+++ fabfile/config.py
@@ -6,7 +6,7 @@ from os.path import abspath, dirname, join, realpath
 from fabric.api import env
 
 env.root_dir = abspath(join(dirname(realpath(__file__)), pardir))
-
+env.flask_app = join(env.root_dir, 'autoapp.py')
 env.build_dir = join(env.root_dir, 'build')
 
 env.remote = 'myles@bear.mylesbraithwaite.com:/srv/www/mylesb.ca/gifs/html/'
--- fabfile/flaskcli.py
+++ fabfile/flaskcli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Fabric Flask Commands and Wrapper."""
-from fabric.api import local, task
+from fabric.api import env, local, task
+from fabric.context_managers import shell_env
 
 
 def flask(command, *args, **kwargs):
@@ -13,7 +14,8 @@ def flask(command, *args, **kwargs):
     for key, value in kwargs.items():
         opts.append('--{0}={1}'.format(key, value))
 
-    local('pipenv run flask {0} {1}'.format(command, ' '.join(opts)))
+    with shell_env(FLASK_APP=env.flask_app, FLASK_DEBUG='1'):
+        local('pipenv run flask {0} {1}'.format(command, ' '.join(opts)))
 
 
 @task
